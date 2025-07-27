674a1dc5e97163938a8c1acab68587d67f70608e
deomiarn <deomicarnel@hotmail.com>
2025-07-27 17:14:23 +0800

    refactor: reorganize project structure to monorepo and update configuration files

--- apps/builder-ui/src/main.tsx
+++ apps/builder-ui/src/main.tsx
@@ -1,8 +1,8 @@
+import "./index.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import { RouterProvider } from "react-router-dom";
-import { router } from "@/app/router.tsx";
+import { router } from "@/router/router.js";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
