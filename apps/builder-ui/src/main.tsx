85745716b35c6ed4cfb2c6a9e5fcca1960cd0cc2
deomiarn <deomicarnel@hotmail.com>
2025-07-28 17:08:24 +0800

    refactor: update router import path and rename router file

--- apps/builder-ui/src/main.tsx
+++ apps/builder-ui/src/main.tsx
@@ -2,7 +2,7 @@ import "./index.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
-import { router } from "@/router/router.js";
+import { router } from "@/router.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
